c8b973f231980fae195b86cfe0dcda024066b7e5
ayushi-b <kanchibansal08@gmail.com>
2018-05-27 19:59:45 +0530

    First commit

---
+++ 1/reducer.py
@@ -0,0 +1,28 @@
+#!/usr/local/bin/python3
+
+import sys
+import numpy as np
+
+last_key = None
+running_total = 0
+
+for input_line in sys.stdin:
+
+    input_line = input_line.strip()
+
+    this_key, value = input_line.split("\t")
+
+    value = float(value)
+
+    if last_key == this_key:    
+        running_total += value
+
+    else:
+        if last_key:
+            print( "{0}\t{1}".format(last_key, np.round(running_total, 2)))
+                               
+        running_total = value
+        last_key = this_key
+
+if last_key:
+    print("{0}\t\t{1}".format(last_key, np.round(running_total)))
